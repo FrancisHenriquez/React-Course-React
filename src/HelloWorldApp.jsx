ceddb4700ce343e25c0159972be8a385b76f4e70
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-12 12:43:56 -0400

    primer-componente

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,12 @@
+// export function App (){
+//     return <h1>Hola Mundo!!!!</h1>;
+// }
+
+// export const hola = '1234';
+// import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  )
+}
