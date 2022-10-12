11466d865dd3b14a1a248be379a568bd60868074
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-12 13:19:52 -0400

    estilos globales

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import  ReactDOM  from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 // import { HelloWorldApp } from './HelloWorldApp';
-
-
+import './Index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
