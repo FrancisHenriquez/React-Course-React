a6bc18e7c3c08a7f293b3e54d1cc8aed626bbdd6
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-12 13:14:02 -0400

    retornando funciones

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+const getResult = (a,b) => {
+    return a + b;
+}
 export const FirstApp = () => {
+
+    const newMesage = {
+        message: 'Hola',
+        title:'Francis'
+    }
   return (
-    <> <h1>Francis Henriquez Matos</h1>
+    <> 
+    <h1>{
+        getResult(1,2)
+        }</h1>
+    {/* <h1>{JSON.stringify(newMesage) }</h1> */}
     <p>Subtitulo</p>
     </>
    
